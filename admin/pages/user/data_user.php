4ecdadec9047c67f89bac887a8f8083193ef3caa
fauzanansyori <fauzanansyori122@gmail.com>
2023-11-22 22:37:32 +0700

    Update

--- admin/pages/user/data_user.php
+++ admin/pages/user/data_user.php
@@ -7,25 +7,26 @@
     <title>Healing Yuk | Dashboard</title>
 
     <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
     <script src="https://kit.fontawesome.com/25db4f44a1.js" crossorigin="anonymous"></script>
     <!-- Ionicons -->
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <!-- Tempusdominus Bootstrap 4 -->
-    <link rel="stylesheet" href="../components/css/tempusdominus-bootstrap-4.min.css">
+    <link rel="stylesheet" href="../../components/css/tempusdominus-bootstrap-4.min.css">
     <!-- iCheck -->
-    <link rel="stylesheet" href="../components/css/icheck-bootstrap.min.css">
+    <link rel="stylesheet" href="../../components/css/icheck-bootstrap.min.css">
     <!-- JQVMap -->
-    <link rel="stylesheet" href="../components/css/jqvmap.min.css">
+    <link rel="stylesheet" href="../../components/css/jqvmap.min.css">
     <!-- Theme style -->
-    <link rel="stylesheet" href="../components/css/adminlte.min.css">
+    <link rel="stylesheet" href="../../components/css/adminlte.min.css">
     <!-- overlayScrollbars -->
-    <link rel="stylesheet" href="../components/css/OverlayScrollbars.min.css">
+    <link rel="stylesheet" href="../../components/css/OverlayScrollbars.min.css">
     <!-- Daterange picker -->
-    <link rel="stylesheet" href="../components/css/daterangepicker.css">
+    <link rel="stylesheet" href="../../components/css/daterangepicker.css">
     <!-- summernote -->
-    <link rel="stylesheet" href="../components/css/summernote-bs4.min.css">
+    <link rel="stylesheet" href="../../components/css/summernote-bs4.min.css">
     <!-- DataTables -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/dataTables.bootstrap5.min.css">
@@ -36,9 +37,10 @@
     <div class="wrapper">
 
         <!-- Preloader -->
-        <!-- <div class="preloader flex-column justify-content-center align-items-center">
-      <img class="animation__shake" src="components/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
-    </div> -->
+        <div class="preloader flex-column justify-content-center align-items-center">
+            <img class="animation__shake" src="../../components/img/AdminLTELogo.png" alt="AdminLTELogo" height="60"
+                width="60">
+        </div>
 
         <!-- Navbar -->
         <nav class="main-header navbar navbar-expand navbar-white navbar-light">
@@ -60,8 +62,9 @@
         <!-- Main Sidebar Container -->
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
-            <a href="index3.html" class="brand-link">
-                <img src="../components/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+            <a href="../../index.php" class="brand-link">
+                <img src="../../components/img/AdminLTELogo.png" alt="AdminLTE Logo"
+                    class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">Healing Yuk</span>
             </a>
 
@@ -70,16 +73,18 @@
                 <!-- Sidebar user panel (optional) -->
                 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                     <div class="image">
-                        <img src="../components/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                        <img src="../../components/img/user2-160x160.jpg" class="img-circle elevation-2"
+                            alt="User Image">
                     </div>
                     <div class="info">
-                        <a href="" class="d-block">Admin</a>
+                        <a href="../../index.php" class="d-block">Admin</a>
                     </div>
                 </div>
 
                 <!-- Sidebar Menu -->
                 <nav class="mt-2">
-                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
+                        data-accordion="false">
                         <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
                         <li class="nav-item menu-open">
@@ -92,27 +97,33 @@
                             </a>
                             <ul class="nav nav-treeview">
                                 <li class="nav-item">
-                                    <a href="../index.php" class="nav-link">
+                                    <a href="../../index.php" class="nav-link">
                                         <i class="far fa-circle nav-icon"></i>
                                         <p>Data</p>
                                     </a>
                                 </li>
                                 <li class="nav-item">
-                                    <a href="data_admin.php" class="nav-link active">
+                                    <a href="../admin/data_admin.php" class="nav-link">
                                         <i class="far fa-circle nav-icon"></i>
                                         <p>Admin</p>
                                     </a>
                                 </li>
                                 <li class="nav-item">
-                                    <a href="data_user.php" class="nav-link">
+                                    <a href="data_user.php" class="nav-link active">
                                         <i class="far fa-circle nav-icon"></i>
                                         <p>User</p>
                                     </a>
                                 </li>
                                 <li class="nav-item">
-                                    <a href="#" class="nav-link">
+                                    <a href="../daerah/data_daerah.php" class="nav-link">
                                         <i class="far fa-circle nav-icon"></i>
-                                        <p>Tempat Wisata</p>
+                                        <p>Daerah</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="../kategori/data_kategori.php" class="nav-link">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>Kategori</p>
                                     </a>
                                 </li>
                             </ul>
@@ -131,12 +142,12 @@
                 <div class="container-fluid">
                     <div class="row mb-2">
                         <div class="col-sm-6">
-                            <h1 class="m-0">Data Admin</h1>
+                            <h1 class="m-0">Data User</h1>
                         </div><!-- /.col -->
                         <div class="col-sm-6">
                             <ol class="breadcrumb float-sm-right">
                                 <li class="breadcrumb-item"><a href="#">Home</a></li>
-                                <li class="breadcrumb-item active">Data Admin</li>
+                                <li class="breadcrumb-item active">Data User</li>
                             </ol>
                         </div><!-- /.col -->
                     </div><!-- /.row -->
@@ -148,7 +159,58 @@
             <section class="content">
                 <div class="container-fluid">
                     <!-- /.row -->
-
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="card">
+                                <!-- /.card-header -->
+                                <div class="card-body">
+                                    <table id="example" class="table table-bordered table-striped">
+                                        <thead>
+                                            <tr class="text-center">
+                                                <th>No</th>
+                                                <th>Nama Lengkap</th>
+                                                <th>Username</th>
+                                                <th>Password</th>
+                                                <th>Aksi</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php
+                                            include "../../../public/config/connection.php";
+                                            $query = mysqli_query($connect, "SELECT * FROM user ORDER BY id_nama_user ASC;");
+                                            while ($data = mysqli_fetch_array($query)) {
+                                                ?>
+                                                <tr>
+                                                    <td>
+                                                        <?php echo $data['id_nama_user']; ?>
+                                                    </td>
+                                                    <td>
+                                                        <?php echo $data['nama_lengkap_user']; ?>
+                                                    </td>
+                                                    <td>
+                                                        <?php echo $data['username']; ?>
+                                                    </td>
+                                                    <td>
+                                                        <?php echo $data['password']; ?>
+                                                    </td>
+                                                    <td>
+                                                        <a href="f_hapus_user.php?id=<?php echo $data["id_nama_user"] ?>"
+                                                            class="btn btn-danger"><i class="fa-solid fa-trash"></i></a>
+                                                    </td>
+                                                </tr>
+                                                <?php
+                                            }
+                                            ?>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <!-- /.card-body -->
+                            </div>
+                            <!-- /.card -->
+                        </div>
+                        <!-- /.col -->
+                        <!-- /.row -->
+                    </div>
                     <!-- /.container-fluid -->
                 </div><!-- /.container-fluid -->
             </section>
@@ -158,37 +220,37 @@
     <!-- ./wrapper -->
 
     <!-- jQuery -->
-    <script src="components/js/jquery.min.js"></script>
+    <script src="../../components/js/jquery.min.js"></script>
     <!-- jQuery UI 1.11.4 -->
-    <script src="components/js/jquery-ui.min.js"></script>
+    <script src="../../components/js/jquery-ui.min.js"></script>
     <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
     <script>
         $.widget.bridge('uibutton', $.ui.button)
     </script>
     <!-- Bootstrap 4 -->
-    <script src="../components/js/bootstrap.bundle.min.js"></script>
+    <script src="../../components/js/bootstrap.bundle.min.js"></script>
     <!-- ChartJS -->
-    <script src="../components/js/Chart.min.js"></script>
+    <script src="../../components/js/Chart.min.js"></script>
     <!-- Sparkline -->
-    <script src="../components/js/sparkline.js"></script>
+    <script src="../../components/js/sparkline.js"></script>
     <!-- JQVMap -->
-    <script src="../components/js/jquery.vmap.min.js"></script>
-    <script src="../components/js/jquery.vmap.usa.js"></script>
+    <script src="../../components/js/jquery.vmap.min.js"></script>
+    <script src="../../components/js/jquery.vmap.usa.js"></script>
     <!-- jQuery Knob Chart -->
-    <script src="../components/js/jquery.knob.min.js"></script>
+    <script src="../../components/js/jquery.knob.min.js"></script>
     <!-- daterangepicker -->
-    <script src="../components/js/moment.min.js"></script>
-    <script src="../components/js/daterangepicker.js"></script>
+    <script src="../../components/js/moment.min.js"></script>
+    <script src="../../components/js/daterangepicker.js"></script>
     <!-- Tempusdominus Bootstrap 4 -->
-    <script src="../components/js/tempusdominus-bootstrap-4.min.js"></script>
+    <script src="../../components/js/tempusdominus-bootstrap-4.min.js"></script>
     <!-- Summernote -->
-    <script src="../components/js/summernote-bs4.min.js"></script>
+    <script src="../../components/js/summernote-bs4.min.js"></script>
     <!-- overlayScrollbars -->
-    <script src="../components/js/jquery.overlayScrollbars.min.js"></script>
+    <script src="../../components/js/jquery.overlayScrollbars.min.js"></script>
     <!-- AdminLTE App -->
-    <script src="../components/js/adminlte.js"></script>
+    <script src="../../components/js/adminlte.js"></script>
     <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-    <script src="../components/js/dashboard.js"></script>
+    <script src="../../components/js/dashboard.js"></script>
 
     <!-- DataTables  & Plugins -->
     <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
