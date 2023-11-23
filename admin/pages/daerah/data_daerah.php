4d8a9de2ca9b39848df7f00561c400e809c9967b
maieka <maiekasari12@gmail.com>
2023-11-23 22:00:52 +0700

    update

--- admin/pages/daerah/data_daerah.php
+++ admin/pages/daerah/data_daerah.php
@@ -185,8 +185,8 @@
                                                     <td><?php echo $no++; ?></td>
                                                     <td><?php echo $data['nama_daerah']; ?></td>
                                                     <td>
-                                                        <a href="edit_daerah.php" class="btn btn-warning"><i class="fa-solid fa-pen-to-square"></i></a>
-                                                        <a href="f_hapus.php" class="btn btn-danger"><i class="fa-solid fa-trash"></i></a>
+                                                        <a href="edit_daerah.php?id_nama_daerah=<?php echo $data['id_nama_daerah'] ?>" class="btn btn-warning"><i class="fa-solid fa-pen-to-square"></i></a>
+                                                        <button type=" button" class="btn btn-danger" onclick="confirmDelete('<?php echo $data["id_nama_daerah"] ?>')"><i class=" fa-solid fa-trash"></i></button>
                                                     </td>
                                                 </tr>
                                             <?php
