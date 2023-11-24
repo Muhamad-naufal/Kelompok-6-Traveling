b6ce703ef00fb1e94d283ee3147c0fd9767d6304
maieka <maiekasari12@gmail.com>
2023-11-24 16:53:14 +0700

    update

--- public/kategori.php
+++ public/kategori.php
@@ -1,7 +1,7 @@
 <div class="container-xxl py-5">
     <div class="container">
         <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
-            <h6 class="section-title bg-white text-center text-primary px-3">Kategori</h6>
+            <h6 class="section-title bg-white text-center text-primary px-3">Category</h6>
             <h1 class="mb-5"></h1>
         </div>
         <div class="row g-4">
