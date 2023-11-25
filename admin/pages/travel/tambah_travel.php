e337554ec11a2343233fe2f03d40c461da94714d
annisa <anisasumayyah26@gmail.com>
2023-11-25 07:19:59 +0700

    update sidebar

--- admin/pages/travel/tambah_travel.php
+++ admin/pages/travel/tambah_travel.php
@@ -113,7 +113,7 @@
         <!-- Main Sidebar Container -->
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
-            <a href="index3.html" class="brand-link">
+            <a href="index3.html" class="brand-link" style="text-decoration: none;">
                 <img src="../../components/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">Healing Yuk</span>
             </a>
@@ -126,7 +126,7 @@
                         <img src="../../components/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
                     </div>
                     <div class="info">
-                        <a href="" class="d-block">Admin</a>
+                        <a href="" class="d-block" style="text-decoration: none;">Admin</a>
                     </div>
                 </div>
 
