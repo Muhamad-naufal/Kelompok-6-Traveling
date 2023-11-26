92e9332624cc27e836af6c7b49d0b3a6fd7e672d
annisa <anisasumayyah26@gmail.com>
2023-11-26 20:04:49 +0700

    validasi duplikat username register user

--- public/proses_register.php
+++ public/proses_register.php
@@ -5,18 +5,18 @@ $nama_lengkap_user = $_POST['nama_lengkap_user'];
 $username = $_POST['username'];
 $password = md5($_POST['password']);
 
-// echo "INSERT INTO user (nama_lengkap_user, username, password) VALUES ('$nama_lengkap_user', '$username', '$password')";
+//cek username
+$sql_cek_username = mysqli_query($connect, "SELECT * FROM `user` WHERE username = '$username';") or die(mysqli_error($connect));
 
-$query = mysqli_query($connect, "INSERT INTO user (nama_lengkap_user, username, password) VALUES ('$nama_lengkap_user', '$username', '$password')");
-
-if ($query) {
+if (mysqli_num_rows($sql_cek_username) > 0) { //jika data sudah ada
     echo "<script>
-    alert('Berhasil Register...');
+    alert('Username Sudah Tedaftar...');
     window.location.href='index.php';
     </script>";
 } else {
+    $query = mysqli_query($connect, "INSERT INTO user (nama_lengkap_user, username, password) VALUES ('$nama_lengkap_user', '$username', '$password')");
     echo "<script>
-    alert('Email atau password salah...');
+    alert('Berhasil Register...');
     window.location.href='index.php';
     </script>";
 }
