1ac42f8991c866e1d56305f3cec89d6a295a402a
annisa <anisasumayyah26@gmail.com>
2023-11-27 17:14:37 +0700

    frond end data.php

--- admin/data.php
+++ admin/data.php
@@ -8,7 +8,6 @@ if (!isset($_SESSION["username"])) {
   exit();
 }
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -37,19 +36,22 @@ if (!isset($_SESSION["username"])) {
   <link rel="stylesheet" href="components/css/daterangepicker.css">
   <!-- summernote -->
   <link rel="stylesheet" href="components/css/summernote-bs4.min.css">
+  <!-- DataTables -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/dataTables.bootstrap5.min.css">
+
 </head>
 
 <body class="hold-transition sidebar-mini layout-fixed">
   <div class="wrapper">
-
-    <!-- Preloader -->
+    <!-- Preloader Start -->
     <div class="preloader flex-column justify-content-center align-items-center">
-      <img class="animation__shake" src="components/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+      <img class="animation__shake" src="../../components/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
     </div>
+    <!-- Preloader End -->
 
-    <!-- Navbar -->
+    <!-- Navbar Start -->
     <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-      <!-- Left navbar links -->
       <ul class="navbar-nav">
         <li class="nav-item">
           <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
@@ -62,31 +64,28 @@ if (!isset($_SESSION["username"])) {
         </li>
       </ul>
     </nav>
-    <!-- /.navbar -->
+    <!-- Navbar End -->
 
-    <!-- Main Sidebar Container -->
+    <!-- Main Sidebar Container Start -->
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <!-- Brand Logo -->
-      <a href="" class="brand-link">
+      <a href="data.php" class="brand-link" style="text-decoration: none;">
         <img src="components/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">Healing Yuk</span>
       </a>
 
-      <!-- Sidebar -->
+      <!-- Sidebar Start -->
       <div class="sidebar">
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
           <div class="info">
-            <a href="" class="d-block"><?php echo $_SESSION["username"] ?></a>
+            <a href="" class="d-block" style="text-decoration: none;"><?php echo $_SESSION["username"] ?></a>
           </div>
-
         </div>
 
-        <!-- Sidebar Menu -->
+        <!-- Sidebar Menu Start -->
         <nav class="mt-2">
           <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-            <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
             <li class="nav-item menu-open">
               <a href="#" class="nav-link active">
                 <i class="nav-icon fas fa-tachometer-alt"></i>
@@ -127,13 +126,13 @@ if (!isset($_SESSION["username"])) {
                   </a>
                 </li>
                 <li class="nav-item">
-                  <a href="pages/travel/data_travel.php" class="nav-link">
+                  <a href="pages//travel/data_travel.php" class="nav-link">
                     <i class="nav-icon fas fa-map-location-dot"></i>
                     <p>Tempat Wisata</p>
                   </a>
                 </li>
                 <li class="nav-item">
-                  <a href="pages/review/data_review.php" class="nav-link">
+                  <a href="pages//review/data_review.php" class="nav-link">
                     <i class="nav-icon fas fa-comment-dots"></i>
                     <p>Review</p>
                   </a>
@@ -142,40 +141,160 @@ if (!isset($_SESSION["username"])) {
             </li>
           </ul>
         </nav>
-        <!-- /.sidebar-menu -->
+        <!-- Sidebar Menu End -->
       </div>
-      <!-- /.sidebar -->
+      <!-- Sidebar End -->
     </aside>
+    <!-- Main Sidebar Container End -->
 
-    <!-- Content Wrapper. Contains page content -->
+    <!-- Content Wrapper Page Start -->
     <div class="content-wrapper">
-      <!-- Content Header (Page header) -->
+      <!-- Header Content Start -->
       <div class="content-header">
         <div class="container-fluid">
           <div class="row mb-2">
             <div class="col-sm-6">
               <h1 class="m-0">Data Master</h1>
-            </div><!-- /.col -->
+            </div>
+
             <div class="col-sm-6">
               <ol class="breadcrumb float-sm-right">
                 <li class="breadcrumb-item"><a href="data.php">Home</a></li>
-                <li class="breadcrumb-item active">Data Master</li>
+                <li class="breadcrumb-item active">Data Admin</li>
               </ol>
-            </div><!-- /.col -->
-          </div><!-- /.row -->
-        </div><!-- /.container-fluid -->
-      </div>
-      <!-- /.content-header -->
+            </div>
 
-      <!-- Main content -->
+          </div>
+        </div>
+      </div>
+      <!-- Header Content End -->
+
+      <!-- Main Content Start -->
       <section class="content">
         <div class="container-fluid">
-        </div><!-- /.container-fluid -->
+          <div class="row">
+            <div class="col-md-4">
+              <div class="card text-bg-secondary ">
+                <div class="card-body">
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="me-3">
+                      <div class="text-white small">
+                        <h6>Daerah</h6>
+                      </div>
+                      <div class="text-lg fw-bold"></div>
+                    </div>
+                    <i class="nav-icon fas fa-location fa-xl"></i>
+                  </div>
+                  <div>
+                    17
+                  </div>
+                </div>
+                <div class="card-footer d-flex align-items-center justify-content-between small">
+                  <a class="text-white" href="pages/daerah/data_daerah.php" style="text-decoration: none;">Selengkapnya</a>
+                  <div class="text-white"></div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-4">
+              <div class="card text-bg-secondary ">
+                <div class="card-body">
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="me-3">
+                      <div class="text-white small">
+                        <h6>Kategori</h6>
+                      </div>
+                      <div class="text-lg fw-bold"></div>
+                    </div>
+                    <i class="nav-icon fas fa-list fa-xl"></i>
+                  </div>
+                  <div>
+                    17
+                  </div>
+                </div>
+                <div class="card-footer d-flex align-items-center justify-content-between small">
+                  <a class="text-white" href="pages/kategori/data_kategori.php" style="text-decoration: none;">Selengkapnya</a>
+                  <div class="text-white"></div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-4">
+              <div class="card text-bg-secondary ">
+                <div class="card-body">
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="me-3">
+                      <div class="text-white small">
+                        <h6>Tempat Wisata</h6>
+                      </div>
+                      <div class="text-lg fw-bold"></div>
+                    </div>
+                    <i class="nav-icon fas fa-map-location-dot fa-xl"></i>
+                  </div>
+                  <div>
+                    17
+                  </div>
+                </div>
+                <div class="card-footer d-flex align-items-center justify-content-between small">
+                  <a class="text-white" href="pages/travel/data_travel.php" style="text-decoration: none;">Selengkapnya</a>
+                  <div class="text-white"></div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-4">
+              <div class="card text-bg-secondary ">
+                <div class="card-body">
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="me-3">
+                      <div class="text-white small">
+                        <h6>Review</h6>
+                      </div>
+                      <div class="text-lg fw-bold"></div>
+                    </div>
+                    <i class="nav-icon fas fa-comment-dots fa-xl"></i>
+                  </div>
+                  <div>
+                    17
+                  </div>
+                </div>
+                <div class="card-footer d-flex align-items-center justify-content-between small">
+                  <a class="text-white" href="pages/review/data_review.php" style="text-decoration: none;">Selengkapnya</a>
+                  <div class="text-white"></div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-4">
+              <div class="card text-bg-secondary ">
+                <div class="card-body">
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="me-3">
+                      <div class="text-white small">
+                        <h6>User</h6>
+                      </div>
+                      <div class="text-lg fw-bold"></div>
+                    </div>
+                    <i class="nav-icon fas fa-users fa-xl"></i>
+                  </div>
+                  <div>
+                    17
+                  </div>
+                </div>
+                <div class="card-footer d-flex align-items-center justify-content-between small">
+                  <a class="text-white" href="pages/user/data_user.php" style="text-decoration: none;">Selengkapnya</a>
+                  <div class="text-white"></div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
       </section>
-      <!-- /.content -->
+      <!-- Main Content Start -->
     </div>
+    <!-- Content Wrapper Page Start -->
   </div>
-  <!-- ./wrapper -->
 
   <!-- jQuery -->
   <script src="components/js/jquery.min.js"></script>
@@ -209,6 +328,14 @@ if (!isset($_SESSION["username"])) {
   <script src="components/js/adminlte.js"></script>
   <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
   <script src="components/js/dashboard.js"></script>
+
+  <!-- DataTables  & Plugins -->
+  <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+  <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/1.13.7/js/dataTables.bootstrap5.min.js"></script>
+  <script>
+    new DataTable('#example');
+  </script>
 </body>
 
 </html>
