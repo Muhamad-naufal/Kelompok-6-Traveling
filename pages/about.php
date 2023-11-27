04926e894847037a6ae29f488b298fed2f0d1c89
fauzanansyori <fauzanansyori122@gmail.com>
2023-11-27 23:14:09 +0700

    update

--- pages/about.php
+++ pages/about.php
@@ -45,7 +45,7 @@
     <div class="container-fluid position-relative p-0">
         <nav class="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
             <a href="" class="navbar-brand p-0">
-                <h1 class="text-primary m-0"><i class="fa fa-map-marker-alt me-3"></i>Tourist</h1>
+                <h1 class="text-primary m-0"><i class="fa fa-map-marker-alt me-3"></i>Healing Yuk</h1>
                 <!-- <img src="img/logo.png" alt="Logo"> -->
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
@@ -121,7 +121,7 @@
                             <a class="btn btn-square mx-1" href=""><i class="fab fa-instagram"></i></a>
                         </div>
                         <div class="text-center p-4">
-                            <h5 class="mb-0">Muhamad Fauzan Anshory</h5>
+                            <h5 class="mb-0">Muhamad Fauzan Ansyori</h5>
                             <small>Mahasiswa</small>
                         </div>
                     </div>
